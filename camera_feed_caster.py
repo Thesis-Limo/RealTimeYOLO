75467e06c12683c13d8b24d5cefed9564fc58011
Njål Ingersønn Sørland <njaalis2000@gmail.com>
2023-12-10 21:07:34 +0100

    handle reset connections

--- camera_feed_caster.py
+++ camera_feed_caster.py
@@ -4,7 +4,8 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-def image_callback(msg, clientsocket):
+def image_callback(msg):
+    global clientsocket
     try:
         bridge = CvBridge()
         cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -14,21 +15,28 @@ def image_callback(msg, clientsocket):
         print(e)
     except socket.error as e:
         print("Socket error:", e)
-        clientsocket.close()
+        clientsocket = connect()
+
+def connect():
+    global serversocket
+    print("Waiting for a new connection...")
+    clientsocket, address = serversocket.accept()
+    print("Connected to", address)
+    return clientsocket
 
 def start_server():
+    global serversocket, clientsocket
     rospy.init_node('camera_server')
     print("Starting server...")
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind(('localhost', 23001))
     serversocket.listen(5)
-    print("Waiting for a connection...")
-    clientsocket, address = serversocket.accept()
-    rospy.Subscriber("/camera/rgb/image_raw", Image, image_callback, clientsocket)
-    print("Connected to", address)
+    clientsocket = connect()
+    rospy.Subscriber("/camera/rgb/image_raw", Image, image_callback)
     rospy.spin()
     clientsocket.close()
     serversocket.close()
+
 
 if __name__ == '__main__':
     start_server()
